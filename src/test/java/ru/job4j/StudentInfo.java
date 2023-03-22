d45a76c2e58ffaf2d782c3bebe9c97dc24f1d877
Krifed <dimaorex09@mail.ru>
2023-03-22 20:01:49 +0300

    Info

---
+++ src/test/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,9 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Dima Orechov");
+        System.out.println("14.01.2002");
+
+    }
+}
